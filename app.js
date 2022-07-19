debe2a80de6473fbb88165310789e4adc7eb58bd
82108 <dudals789>
2022-07-19 23:41:53 +0900

    수정 관련 확인

--- app.js
+++ app.js
@@ -4,5 +4,46 @@
  * })
  */
 Ext.onReady(function(){
-    Ext.Msg.alert("타이틀","HelloWorld");
+    //Ext.Msg.alert("타이틀","HelloWorld2");
+    Ext.create("Ext.container.Viewport",{
+        renderTo : Ext.getBody(),
+        // 이 컴포넌트를 어디에 넣을 것인가..
+        layout : 'border',
+        border : true,
+        items : [
+            {
+                xtype : "panel",
+                flex : 1,
+                region : 'north',
+                title : "패널1",
+                layout : "border",
+            },
+            {
+                xtype : "panel",
+                flex: 1,
+                region : 'center',
+                title : "패널2",
+            },
+            {
+                xtype : "panel",
+                flex: 1,
+                region : 'east',
+                title : "패널 east",
+            },
+            {
+                xtype : "panel",
+                flex: 1,
+                region : 'west',
+                title : "패널 west",
+            },
+            {
+                xtype : "panel",
+                flex: 1,
+                region : 'south',
+                title : "패널 south",
+            }
+
+            // north, center, south, east, west
+        ]
+    })
 })
